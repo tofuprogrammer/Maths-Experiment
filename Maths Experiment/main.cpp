f268cfd6392761ba193871cd4b0d80e0cdf1c787
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-07 14:06:19 +0000

    Initial Commit

---
+++ Maths Experiment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Maths Experiment
+//
+//  Created by Sam Hart on 07/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
